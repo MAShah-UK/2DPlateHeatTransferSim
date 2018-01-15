11bde2ae2154b07fa5b4b8362c82e42e3067f9d1
MAShah-UK <MAShah-UK@users.noreply.github.com>
2018-01-15 18:55:45 +0000

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,5 @@
 % Title:   MATLAB CW2 - Heat Transfer
 % Group:   30
-% Members: Matahir Abbas Shah, Rory Southward, Sajan Selvamaniy, Thenujan Thuvaraganathan
-% Contact: Matahir.Shah.1@city.ac.uk
 
 % Q1: Draw temperature distribution.
 simulate_2D_heat(2, 1, 0.02, 0.02, 0.05, [0 1 2 5 10]);
